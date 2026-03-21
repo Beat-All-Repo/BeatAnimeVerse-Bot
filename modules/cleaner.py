d4fb69f491285b8b8071dade9551f076db179c6c
Beat-All-Repo <beatanihindistreamhub@gmail.com>
2026-03-21 21:21:29 +0530

    Ultimate @BeatAnime Bot

--- modules/cleaner.py
+++ modules/cleaner.py
@@ -1,3 +1,7 @@
+# ====================================================================
+# PLACE AT: /app/modules/cleaner.py
+# ACTION: Replace existing file
+# ====================================================================
 import html
 
 from telegram import ParseMode, Update
