eb092f69fd0091eec7fb4f75c772f46bedd28fc3
Beat-All-Repo <beatanihindistreamhub@gmail.com>
2026-03-21 21:20:37 +0530

    Ultimate @BeatAnime Bot

--- modules/blacklistusers.py
+++ modules/blacklistusers.py
@@ -1,3 +1,7 @@
+# ====================================================================
+# PLACE AT: /app/modules/blacklistusers.py
+# ACTION: Replace existing file
+# ====================================================================
 import html
 
 from telegram import ParseMode, Update
