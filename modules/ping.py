4dcd3417fabca899b8353f4008f2327608f63ac5
Beat-All-Repo <beatanihindistreamhub@gmail.com>
2026-03-21 21:30:24 +0530

    Ultimate @BeatAnime Bot

--- modules/ping.py
+++ modules/ping.py
@@ -1,3 +1,7 @@
+# ====================================================================
+# PLACE AT: /app/modules/ping.py
+# ACTION: Replace existing file
+# ====================================================================
 import time
 
 from telegram import ParseMode, Update
