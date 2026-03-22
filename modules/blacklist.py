076b9b7875f821697bec458369f42246dd93ab55
Beat-All-Repo <beatanihindistreamhub@gmail.com>
2026-03-22 12:46:52 +0530

    Ultimate @BeatAnime Bot

--- modules/blacklist.py
+++ modules/blacklist.py
@@ -476,7 +476,6 @@ BLACKLIST_DEL_HANDLER = MessageHandler(
     (Filters.text | Filters.command | Filters.sticker | Filters.photo)
     & Filters.chat_type.groups,
     del_blacklist,
-    allow_edit=True,
     run_async=True,
 )
 
